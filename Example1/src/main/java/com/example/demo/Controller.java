0e6948fa5363c2c744016debfe39019ee929992e
rmj100 <rmj24@njit.edu>
2021-10-15 10:23:32 -0400

    Update Controller.java
    added putMapping for "/create"

--- Example1/src/main/java/com/example/demo/Controller.java
+++ Example1/src/main/java/com/example/demo/Controller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class Controller {
         return "My First Spring Application";
     }
     
-    @GetMapping("/create")
+    @PutMapping("/create")
     public List<String> create() {
     	return service.createList();
     }
