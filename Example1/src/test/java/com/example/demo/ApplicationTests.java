7ffcf87093731bf5c01593f38baf634e5f046c40
951152 <951152@host.docker.internal>
2021-10-22 15:46:06 -0500

    added delete test and oneMovie2.json

--- Example1/src/test/java/com/example/demo/ApplicationTests.java
+++ Example1/src/test/java/com/example/demo/ApplicationTests.java
@@ -22,6 +22,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.*;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
@@ -50,15 +51,22 @@ class ApplicationTests {
         return new String(Files.readAllBytes(paths));
     }
 	
-	private void createMockData() throws Exception {
+	private void createMockData1() throws Exception {
         String movieStr = getJSON("resources/oneMovie.json");
         movieObj movie = mapper.readValue(movieStr, movieObj.class);
         this.moiveEx = movie;
         RequestBuilder request = post("/update").contentType(MediaType.APPLICATION_JSON).content(movieStr);
         this.mvc.perform(request);
-
     }
 
+	private void createMockData2() throws Exception {
+		String movieStr = getJSON("resources/oneMovie2.json");
+        movieObj movie = mapper.readValue(movieStr, movieObj.class);
+        this.moiveEx = movie;
+        RequestBuilder request = post("/update").contentType(MediaType.APPLICATION_JSON).content(movieStr);
+        this.mvc.perform(request);
+	}
+	
 	@Test
 	void contextLoads() {
 	}
@@ -92,7 +100,7 @@ class ApplicationTests {
 	//@Transactional
 	@Rollback
 	void testCheckList() throws Exception {
-		this.createMockData();
+		this.createMockData1();
 		
 		RequestBuilder request = get("/viewList");
 		
@@ -105,6 +113,43 @@ class ApplicationTests {
         .andExpect(jsonPath("$[0].actors", is(movie.getActors())))
         .andExpect(jsonPath("$[0].release", is(movie.getRelease())))
         .andExpect(jsonPath("$[0].description", is(movie.getDescription())));
+	}
+	
+	@Test
+	@Rollback
+	void testDeleteFromList() throws Exception {
+		this.createMockData1();
+		movieObj movie1 = this.moiveEx;
+		
+		RequestBuilder viewList = get("/viewList");
+		
+		this.createMockData2();
+		movieObj movie2 = this.moiveEx;
+		
+		
+		this.mvc.perform(viewList)
+		.andExpect(status().isOk())
+        .andExpect(jsonPath("$[0].title", is(movie1.getTitle())))
+        .andExpect(jsonPath("$[0].director", is(movie1.getDirector())))
+        .andExpect(jsonPath("$[0].actors", is(movie1.getActors())))
+        .andExpect(jsonPath("$[0].release", is(movie1.getRelease())))
+        .andExpect(jsonPath("$[0].description", is(movie1.getDescription())))
+        .andExpect(jsonPath("$[1].title", is(movie2.getTitle())))
+		.andExpect(jsonPath("$[1].director", is(movie2.getDirector())))
+        .andExpect(jsonPath("$[1].actors", is(movie2.getActors())))
+        .andExpect(jsonPath("$[1].release", is(movie2.getRelease())))
+        .andExpect(jsonPath("$[1].description", is(movie2.getDescription())));
+		
+		
+		RequestBuilder delete = MockMvcRequestBuilders.delete("/delete/" + movie2.getTitle());
+		
+		this.mvc.perform(delete)
+		.andExpect(status().isOk())
+        .andExpect(jsonPath("$[0].title", is(movie1.getTitle())))
+        .andExpect(jsonPath("$[0].director", is(movie1.getDirector())))
+        .andExpect(jsonPath("$[0].actors", is(movie1.getActors())))
+        .andExpect(jsonPath("$[0].release", is(movie1.getRelease())))
+        .andExpect(jsonPath("$[0].description", is(movie1.getDescription())));
 	}
 
 }
